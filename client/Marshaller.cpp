076a393faeae6c8c0a75f24c34af552fe66df58a
iceice54 <125724815+iceice54@users.noreply.github.com>
2026-02-12 02:26:02 +0800

    feat(client): update marshaller

--- client/Marshaller.cpp
+++ client/Marshaller.cpp
@@ -1,33 +1,114 @@
 #include "Marshaller.h"
 #include <winsock2.h>
+#include <string>
 #include <cstring>
 #include <cstdint>
 
-namespace {
-    std::vector<char> pack(int methodId, int reqId, int accNum, double amount) {
-        uint32_t netM = htonl(static_cast<uint32_t>(methodId));
-        uint32_t netR = htonl(static_cast<uint32_t>(reqId));
-        uint32_t netA = htonl(static_cast<uint32_t>(accNum));
+class Buffer
+{
+public:
+    std::vector<char> data;
 
-        std::vector<char> buf(20);
-
-        char* ptr = buf.data();
-
-        std::memcpy(ptr + 0,  &netM, 4);
-        std::memcpy(ptr + 4,  &netR, 4);
-        std::memcpy(ptr + 8,  &netA, 4);
-        std::memcpy(ptr + 12, &amount, 8);
-
-        return buf;
+    void appendInt(int val)
+    {
+        uint32_t netVal = htonl(static_cast<uint32_t>(val));
+        size_t oldSize = data.size();
+        data.resize(oldSize + 4);
+        std::memcpy(data.data() + oldSize, &netVal, 4);
     }
-}
+
+    void appendDouble(double val)
+    {
+        uint64_t temp;
+        std::memcpy(&temp, &val, 8);
+        uint64_t netVal = htonll(temp);
+
+        size_t oldSize = data.size();
+        data.resize(oldSize + 8);
+        std::memcpy(data.data() + oldSize, &netVal, 8);
+    }
+
+    void appendString(const std::string& str)
+    {
+        appendInt(static_cast<int>(str.size()));
+
+        size_t oldSize = data.size();
+        data.resize(oldSize + str.size());
+        std::memcpy(data.data() + oldSize, str.data(), str.size());
+    }
+};
+
+enum class MethodId : uint32_t {
+    OPEN_ACCOUNT  = 1,
+    CLOSE_ACCOUNT = 2,
+    DEPOSIT       = 3,
+    WITHDRAW      = 4,
+    MONITOR       = 5,
+    // GET_DETAILS   = 6, // Idempotent
+    // TRANSFER      = 7  // Non-Idempotent
+};
 
 namespace Marshaller {
-    std::vector<char> marshallCheckBalance(int reqId, int accId) {
-        return pack(1, reqId, accId, 0.0);
+
+    void appendMethod(Buffer& buf, MethodId id) {
+        buf.appendInt(static_cast<int>(id));
+    }
+
+    std::vector<char> marshallOpenAccount(int reqId, const std::string& name, const std::string& password, const std::string& currency, double initialBalance)
+    {
+        Buffer buf;
+        appendMethod(buf, MethodId::OPEN_ACCOUNT);
+        buf.appendInt(reqId);
+        
+        buf.appendString(name);
+        buf.appendString(password);
+        buf.appendString(currency);
+        buf.appendInt(initialBalance);
+
+        return buf.data;
+    };
+
+    std::vector<char> marshallCloseAccount(int reqId, const std::string& name, int accNum, const std::string& password)
+    {
+        Buffer buf;
+        appendMethod(buf, MethodId::CLOSE_ACCOUNT);
+        buf.appendInt(reqId);
+
+        buf.appendString(name);
+        buf.appendInt(accNum);
+        buf.appendString(password);
+
+        return buf.data;
+    }
+
+    std::vector<char> marshallWithdrawDeposit(int type, int reqId, const std::string& name, int accNum, const std::string& password, const std::string& currency, double amount)
+    {
+        Buffer buf;
+        MethodId method = (type == 0) ? MethodId::WITHDRAW : MethodId::DEPOSIT;
+        appendMethod(buf, method);
+        buf.appendInt(reqId);
+
+        buf.appendString(name);
+        buf.appendInt(accNum);
+        buf.appendString(password);
+        buf.appendString(currency);
+        buf.appendDouble(amount);
+
+        return buf.data;
     }
     
-    std::vector<char> marshallDeposit(int reqId, int accId, double amount) {
-        return pack(2, reqId, accId, amount);
+    std::vector<char> marshallMonitor(int reqId, int durationInSeconds)
+    {
+        Buffer buf;
+        appendMethod(buf, MethodId::MONITOR);
+        buf.appendInt(reqId);
+
+        buf.appendInt(durationInSeconds);
+
+        return buf.data;
     }
-}
+
+    // std::vector<char> marshallCheckBalance(int reqId, int accId) {
+    //     return pack(1, reqId, accId, 0.0, "");
+    // }
+};
--- client/Marshaller.h
+++ client/Marshaller.h
@@ -1,7 +1,17 @@
 #pragma once
+
 #include <vector>
+#include <string>
 
 namespace Marshaller {
-    std::vector<char> marshallCheckBalance(int reqId, int accId);
-    std::vector<char> marshallDeposit(int reqId, int accId, double amount);
-};
+    
+    std::vector<char> marshallOpenAccount(int reqId, const std::string& name, const std::string& password, const std::string& currency, double initialBalance);
+
+    std::vector<char> marshallCloseAccount(int reqId, const std::string& name, int accNum, const std::string& password);
+
+    std::vector<char> marshallDeposit(int reqId, const std::string& name, int accNum, const std::string& password, const std::string& currency, double amount);
+
+    std::vector<char> marshallWithdraw(int reqId, const std::string& name, int accNum, const std::string& password, const std::string& currency, double amount);
+    
+    std::vector<char> marshallMonitor(int reqId, int durationInSeconds);
+}
