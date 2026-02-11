db6475324c1eb54758ae8b244266955ea5276917
iceice54 <125724815+iceice54@users.noreply.github.com>
2026-02-12 02:26:53 +0800

    feat(server): create account class

---
+++ server/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    String name;
+    double balance;
+    String password;
+
+    public Account(String name, double balance, String password) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+    }
+}
